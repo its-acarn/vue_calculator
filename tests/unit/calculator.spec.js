62069cff58afeac0a76c15f47195a536d3ebed16
svvishjs <andreww92@live.com>
2021-01-04 13:40:40 +0000

    added a divide test

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -10,25 +10,31 @@ describe('App.vue', () => {
 
 	it('enterNum changes running total', () => {
 		wrapper.vm.previousTotal = 4;
-		wrapper.vm.add('5');
+		wrapper.vm.add(5);
 		expect(wrapper.vm.runningTotal).to.equal(9);
 	});
 
 	it('should be able to add four plus one', () => {
 		wrapper.vm.previousTotal = 4;
-		wrapper.vm.add('1');
+		wrapper.vm.add(1);
 		expect(wrapper.vm.runningTotal).to.equal(5);
 	});
 
 	it('should be able to subtract four from seven', () => {
 		wrapper.vm.previousTotal = 7;
-		wrapper.vm.subtract('4');
+		wrapper.vm.subtract(4);
 		expect(wrapper.vm.runningTotal).to.equal(3);
 	});
 
 	it('should be able to multiply three by fifteen', () => {
 		wrapper.vm.previousTotal = 3;
-		wrapper.vm.multiply('5');
+		wrapper.vm.multiply(5);
 		expect(wrapper.vm.runningTotal).to.equal(15);
+	});
+
+	it('should be able to divide twenty-one by seven', () => {
+		wrapper.vm.previousTotal = 21;
+		wrapper.vm.divide(7);
+		expect(wrapper.vm.runningTotal).to.equal(3);
 	});
 });
