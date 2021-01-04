bdcfc275e14f729bf592ce00880bc08e71bb374e
svvishjs <andreww92@live.com>
2021-01-04 13:35:20 +0000

    added test of add function

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -1,12 +1,22 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-import App from '@/App.vue'
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import App from '@/App.vue';
 
 describe('App.vue', () => {
-  it('enterNum changes running total', () => {
-    const wrapper = shallowMount(App)
-    wrapper.vm.previousTotal = 4
-    wrapper.vm.add('5');
-    expect(wrapper.vm.runningTotal).to.equal(9)
-  })
-})
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallowMount(App);
+	});
+
+	it('enterNum changes running total', () => {
+		wrapper.vm.previousTotal = 4;
+		wrapper.vm.add('5');
+		expect(wrapper.vm.runningTotal).to.equal(9);
+	});
+
+	it('should be able to add four plus one', () => {
+		wrapper.vm.previousTotal = 4;
+		wrapper.vm.add('1');
+		expect(wrapper.vm.runningTotal).to.equal(5);
+	});
+});
